c5571dc03f9156892d923d30c7b3c7a2ddd3a85e
tattii <yuta.tatti@gmail.com>
2016-08-12 05:24:47 +0900

    fix download script

--- data/download.py
+++ data/download.py
@@ -4,13 +4,17 @@ import os
 date = sys.argv[1]
 source = "http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/gpv/original/"
 
-if (len(date) != 12):
+if len(date) != 12:
     print "invalid date"
     sys.exit()
 
-for level in ["Lsurf", "L-pall"]:
-    for ft in ["00-15", "16-33", "34-39"]:
-        filename = "Z__C_RJTD_" + date + "00_MSM_GPV_Rjp_" + level + "_FH" + ft + "_grib2.bin"
-        url = source + "/".join([date[0:4], date[4:6], date[6:8], filename])
-        os.system("wget " + url)
+filetypes = [
+    "Lsurf_FH00-15", "Lsurf_FH16-33", "Lsurf_FH34-39",
+    "L-pall_FH00-15", "L-pall_FH18-33", "L-pall_FH36-39"
+]
+
+for filetype in filetypes:
+    filename = "Z__C_RJTD_" + date + "00_MSM_GPV_Rjp_" + filetype + "_grib2.bin"
+    url = source + "/".join([date[0:4], date[4:6], date[6:8], filename])
+    os.system("wget " + url)
 
